a096c9d574f49f0afa8332113d73e115328b57d4
gbyleo <110125267+gbyleo@users.noreply.github.com>
2022-11-06 23:48:05 -0300

    6-11ExpJson
    6-11ExpJson

--- ArgProgApp/src/app/componentes/experiencia/experiencia.component.ts
+++ ArgProgApp/src/app/componentes/experiencia/experiencia.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { subscribeOn } from 'rxjs';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+
 
 @Component({
   selector: 'app-experiencia',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
-
-  constructor() { }
+experiencialist:any;
+  constructor(private datosporfolio:PortfolioService) { }
 
   ngOnInit(): void {
-  }
+    this.datosporfolio.obtenerdatos().subscribe(data=>{
+    this.experiencialist=data.experiencia;
+  })
+
+}
 
 }
