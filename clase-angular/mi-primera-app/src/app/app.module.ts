64a8cfb5bb54d2ac2c843f0c7f5a4a999283c99a
gbyleo <110125267+gbyleo@users.noreply.github.com>
2022-11-14 00:31:16 -0300

    CarpetaTutorialHernan
    CarpetaTutorialHernan

---
+++ clase-angular/mi-primera-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonasComponent } from './components/personas/personas.component';
+import { PersonaComponent } from './components/persona/persona.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
