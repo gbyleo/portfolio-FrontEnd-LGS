8a99cac426659ca47e5095ad6e6b6fa1d5fc87d1
gbyleo <110125267+gbyleo@users.noreply.github.com>
2022-11-15 16:25:45 -0300

    vueltaa14-11
    vueltaa14-11

--- clase-angular/mi-primera-app/src/app/components/personas/personas.component.ts
+++ clase-angular/mi-primera-app/src/app/components/personas/personas.component.ts
@@ -23,7 +23,9 @@ export class PersonasComponent implements OnInit {
   ngOnInit(): void {
 
     let persona1= new Persona("Hernan","Borre",30)
+    let persona2= new Persona("4a", "fsd", 22)
     this.personas.push(persona1)
+    this.personas.push(persona2)
     this.personas.push(new Persona("Lala", "Lalala",25))
     this.personas.push(new Persona("Pocho", "Pantera",55))
 
