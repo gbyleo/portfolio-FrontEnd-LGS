b2cb84b6bccbe2ec1d10be6e1f29efb9c8230e2f
gbyleo <110125267+gbyleo@users.noreply.github.com>
2022-11-18 16:22:37 -0300

    routing18-11
    routing18-11

--- ArgProgApp/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
+++ ArgProgApp/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+
+
+
+//importamos nuestros componentes//
+
+import { EncabezadoComponent } from '../componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from '../componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from '../componentes/experiencia/experiencia.component';
+import { EducacionComponent } from '../componentes/educacion/educacion.component';
+import { SkillsComponent } from '../componentes/skills/skills.component';
+import { HabilidadesComponent } from '../componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from '../componentes/proyectos/proyectos.component';
+
+
+
+
+
+
+//definicion de nuestras rutas
 const routes: Routes = [
+ {path:'encabezado', component: EncabezadoComponent},
+ {path:'AcercaDe', component: AcercaDeComponent},
+ {path:'Experiencia', component: EducacionComponent},
+ {path:'Skills', component: SkillsComponent},
+ {path:'Habilidades', component: HabilidadesComponent},
+ {path:'Proyectos', component: ProyectosComponent},
 
 ];
 
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes),
-  CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule{ }
